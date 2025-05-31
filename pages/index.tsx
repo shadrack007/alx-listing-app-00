11c33516dde51d654d745387e74fa21b0fa85756
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-05-31 15:09:17 +0300

    apply uniform font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
+import { Quicksand } from "next/font/google";
+
+import FilterSection from "@/components/common/FilterSection";
 import HeroSection from "@/components/common/HeroSection";
+import ListingSection from "@/components/common/ListingSection";
+
+const quicksand = Quicksand({
+  subsets: ["latin"],
+  variable: "--font-quicksand",
+});
 
 const Home: React.FC = () => {
   return (
-    <div className="">
+    <div className={`${quicksand.className} px-3 md:px-5 lg:px-10 space-y-7`}>
       <HeroSection />
+      <FilterSection />
+      <ListingSection />
     </div>
   );
 };
