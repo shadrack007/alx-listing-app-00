7b3ee42f2e0ad407c28e544f5ab50e66e839d299
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-05-31 12:08:54 +0300

    feat: implemented hero section

---
+++ components/common/HeroSection.tsx
@@ -0,0 +1,21 @@
+import {
+  HERO_BACKGROUND_iMAGE_DESKTOP,
+  HERO_BACKGROUND_iMAGE_MOBILE,
+} from "@/constants";
+
+const HeroSection: React.FC = () => {
+  return (
+    <section
+      className={`bg-[url(${HERO_BACKGROUND_iMAGE_MOBILE})] md:bg-[url(${HERO_BACKGROUND_iMAGE_DESKTOP})] h-[286px]  md:h-[421px] lg:h-[481px]  bg-no-repeat bg-center bg-cover text-white text-center flex flex-col justify-center items-center rounded-xl`}
+    >
+      <h1 className="font-semibold text-3xl md:text-[68px] lg:text-[94px]">
+        Find your favorite <br /> place here!
+      </h1>
+      <p className="text-[7.42px] md:text-[18px] lg:[24px]">
+        The best prices for over 2 million properties worldwide
+      </p>
+    </section>
+  );
+};
+
+export default HeroSection;
