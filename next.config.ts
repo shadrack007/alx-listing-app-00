6b5d27222b9b4ec24619611ae7be1752256425e1
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-05-31 15:15:43 +0300

    removed image domains

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  images: {
-    domains: ["example.com"],
-  },
 };
 
 export default nextConfig;
