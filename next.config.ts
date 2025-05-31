be5faf40b5c27430b981eddc37fcd286f0a02481
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-05-31 14:10:00 +0300

    configure next to use images from example.com

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ["example.com"],
+  },
 };
 
 export default nextConfig;
