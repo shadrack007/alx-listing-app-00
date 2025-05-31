36b973bf7008804d8805c4b326aec501bb52688a
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-05-31 12:10:22 +0300

    addeed layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import Layout from "@/components/common/layout/Layout";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  console.log("component", Component);
+  console.log("pageProps", pageProps);
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
