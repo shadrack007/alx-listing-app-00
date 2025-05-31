674afa7829c5b891179ad7f8ab68d6982ea22a45
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-05-31 11:06:18 +0300

    feat: defined property props interface

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -7,3 +7,26 @@ export interface ButtonProps {
   label: string;
   onClick: () => void;
 }
+
+export interface PropertyProps {
+  name: string;
+  address: AddressProps;
+  rating: number;
+  category: string[];
+  price: number;
+  offers: Offers;
+  image: string;
+  discount: string;
+}
+
+interface AddressProps {
+  state: string;
+  city: string;
+  country: string;
+}
+
+interface Offers {
+  bed: string;
+  shower: string;
+  occupants: string;
+}
