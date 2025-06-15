2b3b8ccab62054d2776d3e8e6fb97bb5a16585af
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-15 11:55:29 +0300

    added the domains for images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ["images.unsplash.com", "images.pexels.com"],
+  },
 };
 
 export default nextConfig;
